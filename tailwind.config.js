89db350b52ab80bf153adf8feea9e682b0a59c8a
Pradeep <pradeep.github.io@gmail.com>
2022-05-16 09:06:03 +0530

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
